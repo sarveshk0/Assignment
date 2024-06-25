53f2a6c88b84ecc024127bfdf8b26c86454a6899
sarveshk0 <kumarsarvesh105@gmail.com>
2024-06-25 18:10:17 +0530

    new

--- src/app/_components/PopupMenu.js
+++ src/app/_components/PopupMenu.js
@@ -3,12 +3,13 @@ import './PopupMenu.css'; // optional for styling
 
 const PopupMenu = () => {
   return (
-    <div className="popup-menu">
+
+    <div className="popup-menu text-black">
       <ul>
         <li>People - Community</li>
         <li>Places - Venues</li>
         <li>Programs - Events</li>
-      <li> Products - Store</li>
+        <li> Products - Store</li>
       <li> Blogs</li>
       </ul>
     </div>
